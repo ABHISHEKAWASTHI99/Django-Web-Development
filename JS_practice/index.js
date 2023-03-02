3de02b4b7d635678f5b7bfe096e247c98e2d9c3a
Abhishek Awasthi <abhiawasthi220999@gmail.com>
2023-03-02 20:28:00 +0530

    some file location changed and some new files added.

---
+++ JS_practice/index.js
@@ -0,0 +1,17 @@
+console.log();('It works')
+
+updateColor= function(){
+    console.log('hail hydra')
+}
+
+function doSomething(){
+    alert('i am doing something')
+}
+
+animate_button= () => {
+    console.log('I am animating the button')
+}
+
+changeMessage = (e) => {
+    e.target.innerText='Button Clicked'
+}
